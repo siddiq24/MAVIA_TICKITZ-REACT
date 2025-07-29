49adb4acb1e32288fbfbfbef5ef121bf6776b1f2
siddiq24 <siddiqrm24@gmail.com>
2025-07-30 06:01:24 +0700

    feat: add routes to all pages for easy navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import React from "react"
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import LoginPage from "./pages/Login-Page"
+import LandingPage from "./pages/Landing-Page.jsx"
+import RegistrasiPage from "./pages/Registrasi-Page.jsx"
+import MoviesListPage from "./pages/MoviesList-Page.jsx"
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/Registrasi" element={<RegistrasiPage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/Movies" element={<MoviesListPage />} />
       </Routes>
     </BrowserRouter>
   )
