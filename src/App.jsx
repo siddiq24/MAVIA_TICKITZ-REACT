53e9190aeee0b4d2536943e21d6ed8babec15b3c
siddiq24 <siddiqrm24@gmail.com>
2025-07-31 06:19:34 +0700

    feat: implementation templating layout using react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,36 @@
 import React from "react"
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Outlet } from 'react-router-dom'
 import LoginPage from "./pages/Login-Page"
 import LandingPage from "./pages/Landing-Page.jsx"
 import RegistrasiPage from "./pages/Registrasi-Page.jsx"
 import MoviesListPage from "./pages/MoviesList-Page.jsx"
+import Navbar from "./components/Navbar.jsx"
+import DetailPage from './pages/DetailPage.jsx'
+import OrderPage from './pages/OrderPage.jsx'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<LandingPage />} />
         <Route path="/Registrasi" element={<RegistrasiPage />} />
         <Route path="/login" element={<LoginPage />} />
-        <Route path="/Movies" element={<MoviesListPage />} />
+        <Route element={<LandingLayout />}>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/Movies" element={<MoviesListPage />} />
+          <Route path="/Detail" element={<DetailPage />} />
+          <Route path="/Order" element={<OrderPage />} />
+        </Route>
       </Routes>
     </BrowserRouter>
+  )
+}
+
+function LandingLayout(){
+  return(
+    <>
+      <Navbar/>
+      <Outlet/>
+    </>
   )
 }
 
